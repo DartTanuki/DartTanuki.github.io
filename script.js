f4f6fa88103c8feeb9335d73574b941a7fd62c5b
Nikita <tanukids@yandex.ru>
2024-08-04 01:30:07 +0300

    Added Buy Modal

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // MAIN LOGIC
+    const buyTrigger = document.querySelector('#buy-ton'),
+          closeBuyModal = document.querySelector('.buy-modal-close');
+          buyTonModal = document.querySelector('.buy-modal-background');
+
+    const openBuyModal = function() {
+        buyTonModal.classList.add('show-modal');
+        buyTonModal.classList.remove('close-modal');
+    }
+
+    const closeBuyModalFunc = function() {
+        console.log('Logged');
+        buyTonModal.classList.add('close-modal');
+        buyTonModal.classList.remove('show-modal');
+    }
+    buyTrigger.addEventListener('click', (event) => {
+        if (event.target === buyTrigger) {
+            openBuyModal();
+        }
+    });
+
+    closeBuyModal.addEventListener('click', closeBuyModalFunc);
+});
