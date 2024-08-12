7fa025ebe129c24a7fa3e0a2741c77b5e45039d2
Nikita <tanukids@yandex.ru>
2024-08-12 13:06:15 +0300

    BL

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 import TonConnectUI from '@tonconnect/ui'
 
-document.addEventListener("DOMContentLoaded", () => {
-    // MAIN LOGIC
-    const buyTrigger = document.querySelector('#buy-ton'),
-          closeBuyModal = document.querySelector('.buy-modal-close');
-          buyTonModal = document.querySelector('.buy-modal-background');
-
-    const openBuyModal = function() {
-        buyTonModal.classList.add('show-modal');
-        buyTonModal.classList.remove('close-modal');
-    }
-
-    const closeBuyModalFunc = function() {
-        buyTonModal.classList.add('close-modal');
-        buyTonModal.classList.remove('show-modal');
-    }
-    
-    buyTrigger.addEventListener('click', (event) => {
-        if (event.target === buyTrigger) {
-            openBuyModal();
-        }
-    });
-
-    closeBuyModal.addEventListener('click', closeBuyModalFunc);
+const tonConnectUI = new TonConnectUI({
+    manifestUrl: 'https://darttanuki.github.io/tonconnect-manifest.json',
+    buttonRootId: 'ton-connect'
 });
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//     // MAIN LOGIC
+//     const buyTrigger = document.querySelector('#buy-ton'),
+//           closeBuyModal = document.querySelector('.buy-modal-close');
+//           buyTonModal = document.querySelector('.buy-modal-background');
+
+//     const openBuyModal = function() {
+//         buyTonModal.classList.add('show-modal');
+//         buyTonModal.classList.remove('close-modal');
+//     }
+
+//     const closeBuyModalFunc = function() {
+//         buyTonModal.classList.add('close-modal');
+//         buyTonModal.classList.remove('show-modal');
+//     }
+    
+//     buyTrigger.addEventListener('click', (event) => {
+//         if (event.target === buyTrigger) {
+//             openBuyModal();
+//         }
+//     });
+
+//     closeBuyModal.addEventListener('click', closeBuyModalFunc);
+//     const walletsList = await tonConnectUI.getWallets();
+//     console.log(walletsList);
+// });
