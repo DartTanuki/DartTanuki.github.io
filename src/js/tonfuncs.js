80191d33ec23531b638d891c13df58cd254a298b
Nikita <tanukids@yandex.ru>
2024-08-21 16:10:29 +0300

    Testing transactions 6

--- src/js/tonfuncs.js
+++ src/js/tonfuncs.js
@@ -6,9 +6,14 @@ function startTonFunc() {
             validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
             messages: [
                 {
-                    address: to_address,
-                    amount: amount, // Nanotons
+                    address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
+                    amount: "200000",
                  // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
+                },
+                {
+                    address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
+                    amount: "200000",
+                 // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
                 }
             ]
         }
@@ -31,7 +36,7 @@ function startTonFunc() {
         } catch (e) {
             console.error(e);
         }
-    })
+    });
 }
 
 export default startTonFunc;
